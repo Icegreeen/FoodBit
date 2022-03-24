3de97188ff95f080bdc881282460501811c0f722
Icegreeen <flavioaquila16@gmail.com>
2022-03-24 01:05:08 -0300

    finished function - whatsapp config

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 //
 
 let prato;
+let bebida;
+let sobremesa;
 
-function getFrango() {
-  document.getElementById("frango").style.borderColor = "green";
+function getHamburger() {
+  document.getElementById("hamburger").style.borderColor = "green";
   document.getElementById("bisteca").style.borderColor = "white";
-  prato = "Frango";
+  prato = "Hamburger";
 }
 
 function getBisteca() {
   document.getElementById("bisteca").style.borderColor = "green";
-  document.getElementById("frango").style.borderColor = "white";
+  document.getElementById("hamburger").style.borderColor = "white";
+
+  prato = "Bisteca";
 }
 
 function getCoca() {
   document.getElementById("coca").style.borderColor = "green";
   document.getElementById("pepsi").style.borderColor = "white";
+
+  bebida = "Coca";
 }
 
 function getPepsi() {
   document.getElementById("pepsi").style.borderColor = "green";
   document.getElementById("coca").style.borderColor = "white";
+
+  bebida = "Pepsi";
 }
 
 function getSorvete() {
   document.getElementById("sorvete").style.borderColor = "green";
   document.getElementById("mousse").style.borderColor = "white";
+
+  sobremesa = "Sorvete";
 }
 
 function getMousse() {
   document.getElementById("mousse").style.borderColor = "green";
   document.getElementById("sorvete").style.borderColor = "white";
+
+  sobremesa = "Mousse";
+}
+
+function finalizarPedido() {
+  let mensagem;
+  mensagem =
+    "Olá, gostaria de pedir um combo: " +
+    prato +
+    ", " +
+    bebida +
+    " e " +
+    sobremesa;
+
+  window.open("https://wa.me/+71991826903?text=" + mensagem);
 }
